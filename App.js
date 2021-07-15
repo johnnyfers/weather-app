a5f8810719edf86e2a16794c6e4eaca51d086496
johnny <joaovlima07@hotmail.com>
2021-07-15 13:21:57 -0300

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
+const WEATHER_API_KEY = 'cb9eb76e233d73bdd4d4a7f22e6f43cf'
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null)
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function load() {
+    setCurrentWeather(null)
+    setErrorMessage(null)
+
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+
+      if (status !== 'granted') {
+        setErrorMessage('Access to location is needed to run the app')
+        return
+      }
+
+      const location = await Location.getCurrentPositionAsync()
+
+      const { latitude, longitude } = location.coords
+
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+
+      const response = await fetch(weatherUrl)
+
+      const result = await response.json()
+
+      if (response.ok) {
+        setCurrentWeather(result)
+      } else {
+        setErrorMessage(result.message)
+      }
+
+    } catch (err) {
+      setErrorMessage(err.message)
+    }
+  }
+
+  if (currentWeather) {
+    return (
+      <View style={styles.container}>
+        <Text>{currentWeather.main.temp}</Text>
+        <StatusBar style="auto" />
+      </View>)
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>)
+  }
 }
 
 const styles = StyleSheet.create({
